fb561654c4b2ced0055f5552f19add79d8cdadb8
bfattori <bfattori@508b6656-1844-0410-a2e5-a95b844a0cf6>
2008-01-24 16:15:58 +0000

    Forgot to call the base component

--- components/component.mover2d.js
+++ components/component.mover2d.js
@@ -1,7 +1,7 @@
 /**
  * The Render Engine
  * 2DMoverComponent
- * 
+ *
  * A simple mover component that adjusts the translation of a component
  * over time.  This component handles velocity, acceleration, and
  * deceleration.
@@ -10,17 +10,17 @@
  * @version: 0.1
  *
  * Copyright (c) 2008 Brett Fattori (brettf@renderengine.com)
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,21 +30,21 @@
  * THE SOFTWARE.
  *
  */
- 
+
 var Mover2DComponent = Transform2DComponent.extend({
-   
+
    lastTime: -1,
-   
+
    velocity: new Vector2D(0,0),
-   
+
    acceleration: new Vector2D(0,0),
-   
+
    angularVelocity: 0,
-   
+
    execute: function(renderContext, time) {
       var pos = this.getPosition();
       var rot = this.getRotation();
-      
+
       // If we've just come into the world, we can short circuit with a
       // quick addition of the velocity.
       if (this.lastTime == -1)
@@ -57,13 +57,14 @@ var Mover2DComponent = Transform2DComponent.extend({
          var az = acceleration.mul(0.5).mul(time * time);
          pos.add(vz).add(az);
          this.setPosition(pos);
-         
+
          //this.setRotation(rot + angularVelocity * (time - this.lastTime));
       }
-      
+
+      this.base();
       this.lastTime = time;
    },
-   
+
    setVelocity: function(vector) {
       this.velocity.set(vector);
    },
@@ -71,15 +72,15 @@ var Mover2DComponent = Transform2DComponent.extend({
    getVelocity: function() {
       return this.velocity;
    },
-   
+
    setAcceleration: function(vector) {
       this.acceleration.set(vector);
    },
-   
+
    getAcceleration: function() {
       return this.acceleration;
    },
-   
+
    setAngularVelocity: function(angularVelocity) {
       this.angularVelocity = angularVelocity;
    },
@@ -87,7 +88,7 @@ var Mover2DComponent = Transform2DComponent.extend({
    getAngularVelocity: function() {
       return this.angularVelocity;
    },
-   
+
    /**
     * Get the class name of this object
     *
@@ -96,5 +97,5 @@ var Mover2DComponent = Transform2DComponent.extend({
    getClassName: function() {
       return "Mover2DComponent";
    }
-   
+
 });
