b8c25535053311d6c8085de55c6cac4572d6071f
bfattori <bfattori@508b6656-1844-0410-a2e5-a95b844a0cf6>
2008-01-24 16:19:54 +0000

    Any methods that modify a Point2D or Vector2D will return the instance of that object so that method chaining can occur.  ie: vec.add(point).mul(4).normalize();

--- trunk/platform/engine.math2d.js
+++ trunk/platform/engine.math2d.js
@@ -1,24 +1,24 @@
 /**
  * The Render Engine
  * Math2D
- * 
+ *
  * A simple 2D math library.
  *
  * @author: Brett Fattori (brettf@renderengine.com)
  * @version: 0.1
  *
  * Copyright (c) 2008 Brett Fattori (brettf@renderengine.com)
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,41 +30,41 @@
  */
 
 var Math2D = Base.extend({
-   
+
    /**
     * An approximation of PI for speedier calculations.
     */
    PI: 3.14159,
-   
+
    /**
     * An approximation of the inverse of PI so we can
     * avoid divisions.
     */
    INV_PI: 0.31831,
-   
+
    degToRad: function(degrees) {
       // ((Math.PI / 180) * deg);
       return (0.01745 * degrees);
    },
-   
+
    radToDeg: function(radians) {
       // ((rad * 180) / Math.PI)
       return ((rad * 180) * Math2D.INV_PI);
    }
-   
+
 });
 
 /**
  * 2D point class
  */
 var Point2D = Base.extend({
- 
+
    x: 0,
    y: 0,
- 
+
    /**
     * Create a new 2D point.
-    * 
+    *
     * @param x {Point2D/Number} If this arg is a Point2D, its values will be
     *                           copied into the new point.
     * @param y {Number} The Y coordinate of the point.  Only required if X
@@ -83,10 +83,10 @@ var Point2D = Base.extend({
          this.y = y || 0;
       }
    },
-   
+
    /**
     * Set the position of a 2D point.
-    * 
+    *
     * @param x {Point2D/Number} If this arg is a Point2D, its values will be
     *                           copied into the new point.
     * @param y {Number} The Y coordinate of the point.  Only required if X
@@ -104,46 +104,62 @@ var Point2D = Base.extend({
          this.x = x;
          this.y = y || 0;
       }
+
+      return this;
    },
-   
+
    add: function(point) {
       this.x += point.x;
       this.y += point.y;
+
+      return this;
    },
-   
+
    addScalar: function(scalar) {
       this.x += scalar;
       this.y += scalar;
+
+      return this;
    },
-   
+
    sub: function(point) {
       this.x -= point.x;
       this.y -= point.y;
+
+      return this;
    },
-   
+
    convolve: function(point) {
       this.x *= point.x;
       this.y *= point.y;
+
+      return this;
    },
-   
+
    convolveInverse: function(point) {
       Assert((point.x != 0 && point.y != 0), "Division by zero in Point.convolveInverse");
-     
+
       this.x /= point.x;
       this.y /= point.y;
+
+      return this;
    },
-   
+
    mul: function(scalar) {
       this.x *= scalar;
       this.y *= scalar;
+
+      return this;
    },
-   
+
    div: function(scalar) {
       Assert((scalar != 0), "Division by zero in Point.divScalar");
       this.x /= scalar;
       this.y /= scalar;
+
+      return this;
    },
-   
+
    /**
     * Negate the point.
     *
@@ -151,6 +167,8 @@ var Point2D = Base.extend({
    neg: function() {
       this.x = -this.x;
       this.y = -this.y;
+
+      return this;
    },
 
    /**
@@ -162,7 +180,7 @@ var Point2D = Base.extend({
    }
 
 });
- 
+
 
 /**
  * 2D vector class
@@ -176,6 +194,8 @@ var Vector2D = Point2D.extend({
       var factor = 1.0 / this.len();
       this.x *= factor;
       this.y *= factor;
+
+      return this;
    },
 
    /**
