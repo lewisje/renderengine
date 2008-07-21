88f108bb5b3d69f49b1733d656272ba26ab89b03
bfattori <bfattori@508b6656-1844-0410-a2e5-a95b844a0cf6>
2008-07-21 16:21:14 +0000

    Updated documentation for clarity.

--- trunk/games/demos/hostDemo.js
+++ trunk/games/demos/hostDemo.js
@@ -38,6 +38,11 @@ Engine.load("/textrender/text.vector.js");
 // Load a transform component
 Engine.load("/components/component.transform2d.js");
 
+/*
+ * We load the HelloWorld object which is relative to
+ * the game.  Examine "helloWorld.js" to see what exactly a
+ * host object is.
+ */
 Game.load("helloWorld.js");
 
 /*
@@ -65,16 +70,24 @@ var HostDemo = BaseObject.extend({
     */
    renderContext: null,
 
+	/*
+	 * These two objects will represent our host objects.  They
+	 * are two instances of the exact same object.
+	 */
    hello1: null,
-
    hello2: null,
 
+	/*
+	 * We'll have a little fun and animate the objects. There
+	 * are many ways to animate an object, with this timer
+	 * being one of the easiest.
+	 */
    timer: null,
 
    constructor: function() {
 
       /*
-       * We call the super class (the Container object) and
+       * We call the super class (the BaseObject object) and
        * pass the name of our object to it.  This way, we can
        * refer to an object in the game engine by name.
        */
@@ -103,12 +116,19 @@ var HostDemo = BaseObject.extend({
     */
    destroy: function() {
 
+		/*
+		 * Anything that we create should be destroyed.  This way we
+		 * can keep our memory clean and allow the garbage collector
+		 * to clean up objects as they become unused.
+		 */
       this.timer.destroy();
-
-      /*
-       * We need to clean up our render context.
-       */
       this.renderContext.destroy();
+
+		/*
+		 * Remember to call the base class, unless you are overriding
+		 * the method.  In the case of the "destroy" method, you'll
+		 * almost always want to call the base class.
+		 */
       this.base();
    },
 
@@ -124,21 +144,74 @@ var HostDemo = BaseObject.extend({
       Engine.getDefaultContext().add(this.renderContext);
       this.renderContext.setBackgroundColor("blue");
 
-
+		/*
+		 * Here we create two instances of our host object. Each instance
+		 * is controlled independent of the other.  We can have as many
+		 * instances of an object as we want, but remember that it takes
+		 * time to process each object and then render it.  More objects on
+		 * screen will result in a real framerate drop.
+		 */
 		this.hello1 = new HelloWorld();
 		this.hello2 = new HelloWorld();
 
+		/*
+		 * Here we add the objects to the rendering context so that they
+		 * will be drawn.  Missing this step will result in your not
+		 * seeing anything, and a lot of headaches!  Some components
+		 * are not rendered, but should still be added to the context
+		 * so they get updated with the rest.
+		 */
 		this.renderContext.add(this.hello1);
 		this.renderContext.add(this.hello2);
 
+		/*
+		 * We're about to create the timer.  It uses what is known as
+		 * an anonymous function.  The function itself has no object scope
+		 * except for itself.  Thus, if we refer to "this" in the anonymous
+		 * function, we are referring to the function.  In this case we
+		 * want to refer to the DemoHost class instance.  To do so, we create
+		 * a reference pointer to the instance and refer to that in the function.
+		 * It sets up what is known as a "closure" (a topic beyond this demo)
+		 * which allows us to access the "this" pointer indirectly.
+		 */
 		var self = this;
-		this.timer = new Interval("HostTimer", 10, function() {
-			var r1 = self.hello1.getRotation();
-			r1 = r1 < 360 ? r1 + 2 : 0;
 
+		/*
+		 * Finally, here we are at the animation timer.  We'll make our
+		 * two host objects rotate in opposite directions.  If you examined the
+		 * host object, you'll notice there were accessor methods for getting
+		 * and setting position, rotation, and scale.  We'll call upon those
+		 * methods of the host object, which will in-turn call the appropriate
+		 * method on the appropriate component.  Every 10 milliseconds our
+		 * timer will update the two objects.
+		 */
+		this.timer = new Interval("HostTimer", 10, function() {
+
+			/*
+			 * Here is that indirect reference I was telling you about.  We
+			 * created a reference to the instance of the DemoHost object and
+			 * called it "self".  So, "self" is actually now a reference to
+			 * the "this" pointer in the scope outside of this anonymous function.
+			 *
+			 * We'll get the rotation of the two host objects and then modify
+			 * them so one rotates clockwise and the other counter clockwise.  We
+			 * set rotations in degrees.
+			 */
+			var r1 = self.hello1.getRotation();
 			var r2 = self.hello2.getRotation();
+
+			/*
+			 * We want to do some "bounds checking" on the two values to keep
+			 * them within a suitable range.  Here we'll be keeping the two
+			 * values between zero and 360 degrees.
+			 */
+			r1 = r1 < 360 ? r1 + 2 : 0;
 			r2 = r2 > 0 ? r2 - 2 : 360;
 
+			/*
+			 * Finally, we set the rotation of the two host objects, which
+			 * will set the rotation on the transform component of each.
+			 */
 			self.hello1.setRotation(r1);
 			self.hello2.setRotation(r2);
 		});
