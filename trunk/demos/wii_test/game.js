a3cec11d42be952154f06d272d02587d9617d9c6
bfattori <bfattori@508b6656-1844-0410-a2e5-a95b844a0cf6>
2009-01-13 22:11:35 +0000

    Fixed up a bug in the BaseObject which was causing failures on load.  Need to find a method to check for syntax errors when loading scripts with the script tag

--- trunk/demos/wii_test/game.js
+++ trunk/demos/wii_test/game.js
@@ -33,7 +33,7 @@
  */
 
 // Load all required engine components
-Engine.include("/rendercontexts/context.htmlelement.js");
+Engine.include("/rendercontexts/context.htmldivcontext.js");
 Engine.include("/resourceloaders/loader.sprite.js");
 Engine.include("/resourceloaders/loader.image.js");
 Engine.include("/engine/engine.timers.js");
@@ -44,125 +44,127 @@ Game.load("/dummy.js");
 
 Engine.initObject("WiiTest", "Game", function(){
 
-	/**
-	 * @class The game.
-	 */
-	var WiiTest = Game.extend({
-	
-		constructor: null,
-		
-		renderContext: null,
-		
-		fieldBox: null,
-		areaScale: 1.0,
-		
-		engineFPS: 15,
-		
-		fieldWidth: 210,
-		fieldHeight: 450,
-		spriteLoader: null,
-		imageLoader: null,
-		
-		
-		/**
-		 * Handle the keypress which starts the game
-		 *
-		 * @param event {Event} The event object
-		 onKeyPress: function(event) {
-		 if (event.keyCode == EventEngine.KEYCODE_ENTER)
-		 {
-		 Spaceroids.startGame();
-		 }
-		 },
-		 */
-		/**
-		 * This method is being used to clean up the demo container.
-		 * Each demo is loaded into this container, and when a demo
-		 * is unloaded we can call this method to clean it up.
-		 */
-		cleanup: function(){
-			this.renderContext.cleanUp();
-		},
-		
-		/**
-		 * Called to set up the game, download any resources, and initialize
-		 * the game to its running state.
-		 */
-		setup: function(){
-		
-			// Set the FPS of the game
-			Engine.setFPS(this.engineFPS);
-			
-			this.spriteLoader = SpriteLoader.create();
-			this.imageLoader = ImageLoader.create();
-			
-			// Load the music
-			//      this.soundLoader.load("bgm", this.getFilePath("resources/smblvl1.mp3"));
-			
-			// Load the card images
-			//      this.imageLoader.load("wild", this.getFilePath("resources/wild.png"), 30, 30);
-			
-			// Don't start until all of the resources are loaded
-			WiiTest.loadTimeout = Timeout.create("wait", 250, WiiTest.waitForResources);
-			this.waitForResources();
-		},
-		
-		/**
-		 * Wait for resources to become available before starting the game
-		 * @private
-		 */
-		waitForResources: function(){
-			if (WiiTest.imageLoader.isReady() &&
-			WiiTest.spriteLoader.isReady()) {
-				WiiTest.loadTimeout.destroy();
-				WiiTest.run();
-				return;
-			}
-			else {
-				// Continue waiting
-				WiiTest.loadTimeout.restart();
-			}
-		},
-		
-		/**
-		 * Called when a game is being shut down to allow it to clean up
-		 * any objects, remove event handlers, destroy the rendering context, etc.
-		 */
-		teardown: function(){
-			this.renderContext.destroy();
-		},
-		
-		run: function(){
-			$("#loading").remove();
-			
-			// Create the render context
-			this.fieldBox = Rectangle2D.create(0, 0, this.fieldWidth, this.fieldHeight);
-			this.centerPoint = this.fieldBox.getCenter();
-			
-			var field = $("<div>").css({
-				width: this.fieldWidth,
-				height: this.fieldHeight,
-				border: "1px solid red",
-				position: "relative"
-			});
-			
-			this.renderContext = HTMLElementContext.create("Playfield", field[0]);
-			Engine.getDefaultContext().add(this.renderContext);
-			WiiTest.play();
-		},
-		
-		play: function(){
-			alert("here");
-			var wimul = WiiHost.create("WiiHost");
-			this.renderContext.add(wimul);
-		},
-		
-		getRenderContext: function(){
-			return this.renderContext;
-		}
-		
-	});
-	
-	return WiiTest;
-	
+   /**
+    * @class The game.
+    */
+   var WiiTest = Game.extend({
+   
+      constructor: null,
+      
+      renderContext: null,
+      
+      fieldBox: null,
+      areaScale: 1.0,
+      
+      engineFPS: 15,
+      
+      fieldWidth: 320,
+      fieldHeight: 400,
+      spriteLoader: null,
+      imageLoader: null,
+      
+      
+      /**
+       * Handle the keypress which starts the game
+       *
+       * @param event {Event} The event object
+       onKeyPress: function(event) {
+       if (event.keyCode == EventEngine.KEYCODE_ENTER)
+       {
+       Spaceroids.startGame();
+       }
+       },
+       */
+      /**
+       * This method is being used to clean up the demo container.
+       * Each demo is loaded into this container, and when a demo
+       * is unloaded we can call this method to clean it up.
+       */
+      cleanup: function(){
+         this.renderContext.cleanUp();
+      },
+      
+      /**
+       * Called to set up the game, download any resources, and initialize
+       * the game to its running state.
+       */
+      setup: function(){
+      
+         // Set the FPS of the game
+         Engine.setFPS(this.engineFPS);
+         
+         this.spriteLoader = SpriteLoader.create();
+         this.imageLoader = ImageLoader.create();
+         
+         // Load the music
+         //      this.soundLoader.load("bgm", this.getFilePath("resources/smblvl1.mp3"));
+         
+         // Load the sprites
+         this.spriteLoader.load("redball", this.getFilePath("resources/redball.js"));
+         
+         // Don't start until all of the resources are loaded
+         WiiTest.loadTimeout = Timeout.create("wait", 250, WiiTest.waitForResources);
+         this.waitForResources();
+      },
+      
+      /**
+       * Wait for resources to become available before starting the game
+       * @private
+       */
+      waitForResources: function(){
+         if (WiiTest.imageLoader.isReady() &&
+         WiiTest.spriteLoader.isReady()) {
+            WiiTest.loadTimeout.destroy();
+            WiiTest.run();
+            return;
+         }
+         else {
+            // Continue waiting
+            WiiTest.loadTimeout.restart();
+         }
+      },
+      
+      /**
+       * Called when a game is being shut down to allow it to clean up
+       * any objects, remove event handlers, destroy the rendering context, etc.
+       */
+      teardown: function(){
+         this.renderContext.destroy();
+      },
+      
+      run: function(){
+         $("#loading").remove();
+         
+         // Create the render context
+         this.fieldBox = Rectangle2D.create(0, 0, this.fieldWidth, this.fieldHeight);
+         this.centerPoint = this.fieldBox.getCenter();
+         
+         var field = $("<div>").css({
+            width: this.fieldWidth,
+            height: this.fieldHeight,
+            border: "1px solid red",
+            position: "relative"
+         });
+         
+         this.renderContext = HTMLDivContext.create("Playfield", this.fieldWidth, this.fieldHeight);
+         this.renderContext.jQ().css({
+            border: "1px solid red",
+            position: "relative"});
+         Engine.getDefaultContext().add(this.renderContext);
+         WiiTest.play();
+      },
+      
+      play: function(){
+         var player = WiiHost.create();
+         this.getRenderContext().add(player);
+      },
+      
+      getRenderContext: function(){
+         return this.renderContext;
+      }
+      
+   });
+   
+   return WiiTest;
+   
 });
