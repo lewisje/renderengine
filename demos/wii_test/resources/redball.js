ccb01305c3aa94af759bff37f26d14a51df1fd2f
bfattori <bfattori@508b6656-1844-0410-a2e5-a95b844a0cf6>
2009-01-13 22:12:35 +0000

    Adding sprite to WiiTest

---
+++ demos/wii_test/resources/redball.js
@@ -0,0 +1,12 @@
+{
+   // Frame (f): left, top, frameWidth, frameHeight
+   // Animation (a): left, top, frameWidth, frameHeight, frameCount, speedMS, loop/toggle
+   bitmapImage: "redball.jpg",
+   bitmapWidth: 120,
+   bitmapHeight: 60,
+   sprites: {
+      "ball": {
+         "a" : [0, 0, 60, 60, 2, 150, "loop"]
+      }
+   }
+}
