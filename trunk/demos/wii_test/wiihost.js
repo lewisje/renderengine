2e85190646097e7a11d2ca1d0bd8347d08988ac1
bfattori <bfattori@508b6656-1844-0410-a2e5-a95b844a0cf6>
2009-01-16 18:12:55 +0000

    Final commit before creating 1.3b tag.  Updated documentation on Wii test game.

--- trunk/demos/wii_test/wiihost.js
+++ trunk/demos/wii_test/wiihost.js
@@ -2,14 +2,14 @@
 /**
  * The Render Engine
  *
- * A block
+ * The player object
  *
  * @author: Brett Fattori (brettf@renderengine.com)
  *
  * @author: $Author$
  * @version: $Revision$
  *
- * Copyright (c) 2008 Brett Fattori (brettf@renderengine.com)
+ * Copyright (c) 2009 Brett Fattori (brettf@renderengine.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -31,6 +31,7 @@
  *
  */
 
+// Load engine objects
 Engine.include("/components/component.collider.js");
 Engine.include("/components/component.wiimoteinput.js");
 Engine.include("/components/component.transform2d.js");
@@ -38,145 +39,181 @@ Engine.include("/engine/engine.object2d.js");
 
 Engine.initObject("WiiHost", "Object2D", function() {
 
-/**
- * @class The block object.  Represents a block which is dropped from
- *        the top of the playfield and is used to build a hand.  Each
- *        block is one of 6 designs.  Each block is comprised of tiles
- *        which are cards that make up a hand, including wild cards.
- */
-var WiiHost = Object2D.extend({
-
-   overBall: null,
-
-   cBox: null,
-   wBox: null,
-
-   constructor: function() {
-      this.base("WiiHost");
-
-      // Add components to move and collide the player
-      this.add(WiimoteInputComponent.create("input"));
-      this.add(Transform2DComponent.create("move"));
-      this.add(ColliderComponent.create("collide", WiiTest.getCModel()));
-      this.setBoundingBox(Rectangle2D.create(0, 0, 60, 60));
-      this.overBall = null;
-
-      this.cBox = $("<div>").css({ position: "absolute", display: "none", border: "1px dashed red"});
-      WiiTest.getRenderContext().getSurface().append(this.cBox);
-
-      this.wBox = $("<div>").css({ position: "absolute", display: "none", border: "2px solid green"});
-      WiiTest.getRenderContext().getSurface().append(this.wBox);
-   },
-
    /**
-    * Update the player within the rendering context.  This draws
-    * the shape to the context, after updating the transform of the
-    * object.  If the player is thrusting, draw the thrust flame
-    * under the ship.
-    *
-    * @param renderContext {RenderContext} The rendering context
-    * @param time {Number} The engine time in milliseconds
+    * @class The block object.  Represents a block which is dropped from
+    *        the top of the playfield and is used to build a hand.  Each
+    *        block is one of 6 designs.  Each block is comprised of tiles
+    *        which are cards that make up a hand, including wild cards.
     */
-   update: function(renderContext, time) {
-      renderContext.pushTransform();
-      this.base(renderContext, time);
-      Engine.addMetric("overBall", this.overBall != null ? "true" : "false");
-      renderContext.popTransform();
+   var WiiHost = Object2D.extend({
 
-      if (Engine.getDebugMode()) {
-         // Find our collision node and put a box around it
-         if (this.ModelData && this.ModelData.lastNode) {
-            var n = this.ModelData.lastNode.getRect().get();
-            this.cBox.css({
+      // The ball the cursor is currently over or null
+      overBall: null,
+
+      // Debugging
+      cBox: null,
+      wBox: null,
+
+      constructor: function() {
+         this.base("WiiHost");
+
+         // Add components to move and collide the player
+         this.add(WiimoteInputComponent.create("input"));
+         this.add(Transform2DComponent.create("move"));
+         this.add(ColliderComponent.create("collide", WiiTest.getCModel()));
+         
+         // The player bounding box
+         this.setBoundingBox(Rectangle2D.create(0, 0, 60, 60));
+         
+         // Initialize the currently selected ball to null
+         this.overBall = null;
+
+         // Debugging
+         this.cBox = $("<div>").css({ position: "absolute", display: "none", border: "1px dashed red"});
+         WiiTest.getRenderContext().getSurface().append(this.cBox);
+         this.wBox = $("<div>").css({ position: "absolute", display: "none", border: "2px solid red"});
+         WiiTest.getRenderContext().getSurface().append(this.wBox);
+      },
+
+      /**
+       * Update the player within the rendering context.  The player doesn't actually have
+       * any shape, so this just update the position and collision model.  Debugging of
+       * the world box and collision node is rendered if debug mode is enabled.
+       *
+       * @param renderContext {RenderContext} The rendering context
+       * @param time {Number} The engine time in milliseconds
+       */
+      update: function(renderContext, time) {
+         renderContext.pushTransform();
+         this.base(renderContext, time);
+         renderContext.popTransform();
+
+         // Use the metrics to let us know if we're over a ball
+         Engine.addMetric("overBall", this.overBall != null ? "true" : "false");
+
+         // Debugging boxes
+         if (Engine.getDebugMode()) {
+            // Find our collision node and put a box around it
+            if (this.ModelData && this.ModelData.lastNode) {
+               var n = this.ModelData.lastNode.getRect().get();
+               this.cBox.css({
+                  display: "block",
+                  left: n.x,
+                  top: n.y,
+                  width: n.w,
+                  height: n.h
+               });
+            }
+
+            var b = this.getWorldBox().get();
+            this.wBox.css({
                display: "block",
-               left: n.x,
-               top: n.y,
-               width: n.w,
-               height: n.h
+               left: b.x,
+               top: b.y,
+               width: b.w,
+               height: b.h
             });
          }
-         
-         var b = this.getWorldBox().get();
-         this.wBox.css({
-            display: "block",
-            left: b.x,
-            top: b.y,
-            width: b.w,
-            height: b.h
-         });
-      }
-   },
+      },
 
-   /**
-    * Get the position of the ship from the mover component.
-    * @type Point2D
-    */
-   getPosition: function() {
-      return this.getComponent("move").getPosition();
-   },
-   
-   getRenderPosition: function() {
-      return this.getPosition();
-   },
+      /**
+       * Get the position of the player from the mover component.
+       * @return {Point2D} The position of the cursor
+       */
+      getPosition: function() {
+         return this.getComponent("move").getPosition();
+      },
 
-   getWorldBox: function() {
-      var bBox = this.base();
-      return bBox.offset(Point2D.create(-30, -30));
-   },
+      /**
+       * Get the render position of the player
+       * @return {Point2D} The rendering position of the cursor
+       */
+      getRenderPosition: function() {
+         return this.getPosition();
+      },
 
-   /**
-    * Set, or initialize, the position of the mover component
-    *
-    * @param point {Point2D} The position to draw the ship in the playfield
-    */
-   setPosition: function(point) {
-      this.base(point);
-      Engine.addMetric("cursorPos", point);
-      this.getComponent("move").setPosition(point);
-   },
+      /**
+       * Get the box which surrounds the player in the world
+       * @return {Rectangle2D} The world bounding box
+       */
+      getWorldBox: function() {
+         var bBox = this.base();
+         return bBox.offset(Point2D.create(-30, -30));
+      },
 
-   onWiimotePosition: function(c, sx, sy, x, y) {
-      if (c == 0) {
-         this.setPosition(Point2D.create(sx, sy));
-      }
-   },
-   
-   onWiimoteButtonA: function(c, state, evt) {
-      if (c == 0 && state) {
-         if (this.overBall) {
-            this.overBall.clicked();
-            return;
-         } else {
-            // Create another ball
-            var b = WiiBall.create();
-            WiiTest.getRenderContext().add(b);
+      /**
+       * Set, or initialize, the position of the mover component
+       * @param point {Point2D} The position where the cursor is
+       */
+      setPosition: function(point) {
+         this.base(point);
+         this.getComponent("move").setPosition(point);
+
+         // Add a metrics value to the display for cursor position
+         Engine.addMetric("cursorPos", point);
+      },
+
+      /**
+       * Respond to the Wii remote position, or the cursor position when not
+       * using a Wii.
+       * @param c {Number} The controller number
+       * @param sx {Number} The screen X position
+       * @param sy {Number} The screen Y position
+       */
+      onWiimotePosition: function(c, sx, sy) {
+         if (c == 0) {
+            // If controller zero, update the position
+            this.setPosition(Point2D.create(sx, sy));
          }
+      },
+
+      /**
+       * Respond to the A button being pressed on the Wii remote, or a mouse
+       * button being pressed or released.
+       * @param c {Number} The controller number
+       * @param state {Boolean} <tt>true</tt> if the button is pressed, <tt>false</tt> when released
+       */
+      onWiimoteButtonA: function(c, state) {
+         if (c == 0 && state) {
+            if (this.overBall) {
+               this.overBall.clicked();
+               return;
+            } else {
+               // Create another ball
+               var b = WiiBall.create();
+               WiiTest.getRenderContext().add(b);
+            }
+         }
+      },
+
+      /**
+       * Check for collision between a ball and the player.
+       * @param obj {Object2D} The object being collided with
+       * @return {Number} A status value
+       * @see {ColliderComponent}
+       */
+      onCollide: function(obj) {
+         if (obj instanceof WiiBall &&
+             (this.getWorldBox().isIntersecting(obj.getWorldBox()))) {
+            this.overBall = obj;
+            return ColliderComponent.STOP;
+         }
+
+         this.overBall = null;
+         return ColliderComponent.CONTINUE;
       }
-   },
-   
-   onCollide: function(obj) {
-      if (obj instanceof WiiBall &&
-          (this.getWorldBox().isIntersecting(obj.getWorldBox()))) {
-         this.overBall = obj;
-         return ColliderComponent.STOP;
+
+   }, { // Static
+
+      /**
+       * Get the class name of this object
+       * @return {String} The string <tt>WiiHost</tt>
+       */
+      getClassName: function() {
+         return "WiiHost";
       }
+   });
 
-      this.overBall = null;
-      return ColliderComponent.CONTINUE;
-   }
-
-}, { // Static
-
-   /**
-    * Get the class name of this object
-    * @return The string <tt>SpriteTest.Actor</tt>
-    * @type String
-    */
-   getClassName: function() {
-      return "WiiHost";
-   }
-});
-
-return WiiHost;
+   return WiiHost;
 
 });
