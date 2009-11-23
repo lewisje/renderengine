4e6fd45ef971219ebd6a0a30ba0961a24f4c7089
bfattori <bfattori@508b6656-1844-0410-a2e5-a95b844a0cf6>
2009-11-23 00:14:19 +0000

    Updated with new keyboard event handlers

--- trunk/components/component.keyboardinput.js
+++ trunk/components/component.keyboardinput.js
@@ -46,6 +46,13 @@ Engine.initObject("KeyboardInputComponent", "InputComponent", function() {
  * <li>onKeyUp - A key was released</li>
  * <li>onKeyPress - A key was pressed and released</li>
  * </ul>
+ * Each function should take up to five arguments.  The first argument is the
+ * <tt>keyCode</tt>, a number which represents which key was acted upon.  See the
+ * {@link EventEngine} for key codes of the non-alphabetic keys. The second argument
+ * is a boolean indicating if the Control key was pressed.  The third argument is a
+ * boolean indicating if the Alt key was pressed.  The fourth argument is a boolean
+ * indicating if the Shift key was pressed.  The fifth and final argument is the
+ * actual event object itself.
  *
  * @extends InputComponent
  */
@@ -95,7 +102,7 @@ var KeyboardInputComponent = InputComponent.extend(/** @scope KeyboardInputCompo
    _keyDownListener: function(eventObj) {
       if (this.getHostObject().onKeyDown)
       {
-         return this.getHostObject().onKeyDown(eventObj);
+         return this.getHostObject().onKeyDown(eventObj.keyCode, eventObj.ctrlKey, eventObj.altKey, eventObj.shiftKey, eventObj);
       }
    },
 
@@ -105,7 +112,7 @@ var KeyboardInputComponent = InputComponent.extend(/** @scope KeyboardInputCompo
    _keyUpListener: function(eventObj) {
       if (this.getHostObject().onKeyUp)
       {
-         return this.getHostObject().onKeyUp(eventObj);
+         return this.getHostObject().onKeyUp(eventObj.keyCode, eventObj.ctrlKey, eventObj.altKey, eventObj.shiftKey, eventObj);
       }
    },
 
@@ -115,7 +122,7 @@ var KeyboardInputComponent = InputComponent.extend(/** @scope KeyboardInputCompo
    _keyPressListener: function(eventObj) {
       if (this.getHostObject().onKeyPress)
       {
-         return this.getHostObject().onKeyPress(eventObj);
+         return this.getHostObject().onKeyPress(eventObj.keyCode, eventObj.ctrlKey, eventObj.altKey, eventObj.shiftKey, eventObj);
       }
    }
 
