27e3bc42d491ee2c406571a95d4ae30a14789b0c
bfattori <bfattori@508b6656-1844-0410-a2e5-a95b844a0cf6>
2010-07-23 22:11:40 +0000

    Tagging release v1.0

---
+++ tags/v1.0/demos/spaceroids/particle.js
@@ -0,0 +1,185 @@
+/**
+ * The Render Engine
+ * Example Game: Spaceroids - an Asteroids clone
+ *
+ * A simple particle
+ *
+ * @author: Brett Fattori (brettf@renderengine.com)
+ *
+ * @author: $Author$
+ * @version: $Revision$
+ *
+ * Copyright (c) 2010 Brett Fattori (brettf@renderengine.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+Engine.include("/engine/engine.math2d.js");
+Engine.include("/engine/engine.particles.js");
+
+Engine.initObject("SimpleParticle", "Particle", function() {
+
+/**
+ * @class A simple particle
+ *
+ * @param pos {Point2D} The starting position of the particle.  A
+ *            velocity vector will be derived from this position.
+ */
+var SimpleParticle = Particle.extend(/** @scope SimpleParticle.prototype */{
+
+   pos: null,
+   vec: null,
+   decel: 0,
+
+   constructor: function(pos, ttl, decel) {
+      this.base(ttl || 2000);
+      this.pos = Point2D.create(pos);
+
+      var a = Math.floor(Math2.random() * 360);
+      this.vec = Math2D.getDirectionVector(Point2D.ZERO, SimpleParticle.ref, a);
+      var vel = 1 + (Math2.random() * (Spaceroids.evolved ? 5 : 2));
+      this.vec.mul(vel);
+      this.decel = decel;
+   },
+
+   destroy: function() {
+      this.pos.destroy();
+      this.vec.destroy();
+      this.base();
+   },
+   
+   release: function() {
+      this.base();
+      this.pos = null;
+      this.vec = null;
+      this.decel = 0;
+   },
+
+   /**
+    * Called by the particle engine to draw the particle to the rendering
+    * context.
+    *
+    * @param renderContext {RenderContext} The rendering context
+    * @param time {Number} The engine time in milliseconds
+    */
+   draw: function(renderContext, time) {
+      if (this.decel > 0 && this.vec.len() > 0) {
+         var invVel = Vector2D.create(this.vec).neg();
+         invVel.mul(this.decel);
+         this.vec.add(invVel);
+         invVel.destroy();
+      }
+      
+      this.pos.add(this.vec);
+      //renderContext.setPosition(this.pos);
+
+      var colr = "#fff";
+      if (Spaceroids.evolved && !Spaceroids.isAttractMode) {
+         var s = time - this.getBirth();
+         var e = this.getTTL() - this.getBirth();
+         colr = 255 - Math.floor(255 * (s / e));
+         colr += (-10 + (Math.floor(Math2.random() * 20)));
+         var fb = (Math2.random() * 100);
+         if (fb > 90) {
+            colr = 255;
+         }
+
+         colr = "#" + (colr.toString(16) + colr.toString(16) + colr.toString(16));
+      }
+
+      renderContext.setFillStyle(colr);
+      renderContext.drawPoint(this.pos);
+   }
+
+}, {
+   getClassName: function() {
+      return "SimpleParticle";
+   },
+
+   // A simple reference point for the "up" vector
+   ref: new Point2D(0, -1)
+});
+
+return SimpleParticle;
+
+});
+
+Engine.initObject("TrailParticle", "Particle", function() {
+
+/**
+ * @class A simple particle
+ *
+ * @param pos {Point2D} The starting position of the particle.  A
+ *            velocity vector will be derived from this position.
+ */
+var TrailParticle = Particle.extend(/** @scope TrailParticle.prototype */{
+
+   pos: null,
+   vec: null,
+   clr: null,
+
+   constructor: function(pos, rot, spread, color, ttl) {
+      this.base(ttl || 2000);
+      this.clr = color;
+      this.pos = Point2D.create(pos);
+      var a = rot + Math.floor((180 - (spread / 2)) + (Math2.random() * (spread * 2)));
+      this.vec = Math2D.getDirectionVector(Point2D.ZERO, TrailParticle.ref, a);
+      var vel = 1 + (Math2.random() * 2);
+      this.vec.mul(vel);
+   },
+
+   destroy: function() {
+      this.pos.destroy();
+      this.vec.destroy();
+      this.base();
+   },
+
+   release: function() {
+      this.base();
+      this.pos = null;
+      this.vec = null;
+   },
+
+   /**
+    * Called by the particle engine to draw the particle to the rendering
+    * context.
+    *
+    * @param renderContext {RenderContext} The rendering context
+    * @param time {Number} The engine time in milliseconds
+    */
+   draw: function(renderContext, time) {
+      this.pos.add(this.vec);
+      //renderContext.setPosition(this.pos);
+      renderContext.setFillStyle(this.clr);
+      renderContext.drawPoint(this.pos);
+   }
+
+}, {
+   getClassName: function() {
+      return "TrailParticle";
+   },
+
+   // A simple reference point for the "up" vector
+   ref: Point2D.create(0, -1)
+});
+
+return TrailParticle;
+
+});
---
+++ tags/v1.0/engine/build/engine.init.js
@@ -0,0 +1,54 @@
+
+/**
+ * The Render Engine
+ * Engine initialization
+ *
+ * @fileoverview Initializes the console and engine
+ *
+ * @author: Brett Fattori (brettf@renderengine.com)
+ * @author: $Author$
+ * @version: $Revision$
+ *
+ * Copyright (c) 2010 Brett Fattori (brettf@renderengine.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+// Start the console so logging can take place immediately
+Console.startup();
+
+// Start the engine
+Engine.startup();
+
+// Set up the engine using whatever query params were passed
+Engine.setDebugMode(EngineSupport.checkBooleanParam("debug"));
+
+if (Engine.getDebugMode())
+{
+   Console.setDebugLevel(EngineSupport.getNumericParam("debugLevel", Console.DEBUGLEVEL_DEBUG));
+}
+
+if (EngineSupport.checkBooleanParam("metrics"))
+{
+   Engine.showMetrics();
+}
+
+// Local mode keeps loaded script source available
+Engine.localMode = EngineSupport.checkBooleanParam("local");
