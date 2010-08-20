0ec562c5d13d0a1a6bd407bf075c172dbd900633
bfattori <bfattori@508b6656-1844-0410-a2e5-a95b844a0cf6>
2010-08-20 19:40:41 +0000

    Updated to 10 integrations per simulated frame

--- physics/physics.simulation.js
+++ physics/physics.simulation.js
@@ -36,125 +36,125 @@ Engine.include("/physics/dynamics/b2World.js");
 
 Engine.initObject("Simulation", "BaseObject", function() {
 
-	/**
-	 * @class A representation of a physical world.  This object is used to
-	 *        introduce Box2dJS physics into your game by creating a world
-	 *        which supports the physical structures provided by Box2dJS.  You
-	 *        will need to create a <tt>Simulation</tt> before you can utilize
-	 *        physics in a game.
-	 *         
-	 * @param name {String} The name of the object
-	 * @param worldBoundary {Rectangle2D} The physical world boundary
-	 * @param [gravity] {Vector2D} The world's gravity vector. default: [0, 300]
-	 * @extends BaseObject
-	 * @constructor
-	 * @description Create a physical world for Box2dJS
-	 */
-	var Simulation = BaseObject.extend({
+   /**
+    * @class A representation of a physical world.  This object is used to
+    *        introduce Box2dJS physics into your game by creating a world
+    *        which supports the physical structures provided by Box2dJS.  You
+    *        will need to create a <tt>Simulation</tt> before you can utilize
+    *        physics in a game.
+    *         
+    * @param name {String} The name of the object
+    * @param worldBoundary {Rectangle2D} The physical world boundary
+    * @param [gravity] {Vector2D} The world's gravity vector. default: [0, 300]
+    * @extends BaseObject
+    * @constructor
+    * @description Create a physical world for Box2dJS
+    */
+   var Simulation = BaseObject.extend({
 
-		worldAABB: null,
-		world: null,
-		gravity: null,
-		doSleep: true,
-		worldBoundary: null,
+      worldAABB: null,
+      world: null,
+      gravity: null,
+      doSleep: true,
+      worldBoundary: null,
 
-		constructor: function(name, worldBoundary, gravity) {
-			this.base(name);
-			this.gravity = gravity || Vector2D.create(0, 450);
-			this.worldAABB = new b2AABB();
-			
-			this.worldBoundary = worldBoundary;
-			var wb = worldBoundary.get();
-			this.worldAABB.minVertex.Set(-1000, -1000);
-			this.worldAABB.maxVertex.Set(1000, 1000);
-			this.doSleep = true;
+      constructor: function(name, worldBoundary, gravity) {
+         this.base(name);
+         this.gravity = gravity || Vector2D.create(0, 450);
+         this.worldAABB = new b2AABB();
+         
+         this.worldBoundary = worldBoundary;
+         var wb = worldBoundary.get();
+         this.worldAABB.minVertex.Set(-1000, -1000);
+         this.worldAABB.maxVertex.Set(1000, 1000);
+         this.doSleep = true;
 
-			var g = this.gravity.get();
-			var grav = new b2Vec2(g.x, g.y);
+         var g = this.gravity.get();
+         var grav = new b2Vec2(g.x, g.y);
 
-			// Create the world
-			this.world = new b2World(this.worldAABB, grav, this.doSleep);		
-		},
-		
-		destroy: function() {
-			this.worldBoundary.destroy();
-			this.gravity.destroy();
-			this.base();
-		},
-		
-		release: function() {
-			this.worldAABB = null;
-			this.gravity = null,
-			this.worldBoundary = null;
-			this.world = null;
-			this.base();
-		},
-		
-		update: function(renderContext, time) {
-			this.world.Step(1/Engine.getFPS(), 1);	
-		},
-		
-		/**
-		 * Support method to get the ground body for the world.
-		 * @return {b2Body} The world's ground body
-		 * @private
-		 */
-		getGroundBody: function() {
-			return this.world.GetGroundBody();
-		},
-		
-		/**
-		 * Support method to add a body to the simulation.  The body must be one of the
-		 * box2d-js body types.  This method is intended to be used by {@link SimulationObject}.
-		 * 
-		 * @param b2jsBody {b2Body} A box2d-js Body object
-		 * @private
-		 */
-		addBody: function(b2jsBody) {
-			return this.world.CreateBody(b2jsBody);
-		},
-		
-		/**
-		 * Support method to add a joint to the simulation.  The joint must be one of the
-		 * box2d-js joint types.  This method is intended to be used by {@link SimulationObject}.
-		 * 
-		 * @param b2jsJoint {b2Joint} A box2d-js Joint object
-		 * @private
-		 */
-		addJoint: function(b2jsJoint) {
-			return this.world.CreateJoint(b2jsJoint);
-		},
-		
-		/**
-		 * Support method to remove a body from the simulation.  The body must be one of the
-		 * box2d-js body types.  This method is intended to be used by {@link SimulationObject}.
-		 * 
-		 * @param b2jsBody {b2Body} A box2d-js Body object
-		 * @private
-		 */
-		removeBody: function(b2jsBody) {
-			this.world.DestroyBody(b2jsBody);
-		},
-		
-		/**
-		 * Support method to remove a joint from the simulation.  The joint must be one of the
-		 * box2d-js joint types.  This method is intended to be used by {@link SimulationObject}.
-		 * 
-		 * @param b2jsJoint {b2Joint} A box2d-js Joint object
-		 * @private
-		 */
-		removeJoint: function(b2jsJoint) {
-			this.world.DestroyJoint(b2jsJoint);
-		}		
-		
-	}, {
-		
-		getClassName: function() {
-			return "Simulation";
-		}
-		
-	});
+         // Create the world
+         this.world = new b2World(this.worldAABB, grav, this.doSleep);     
+      },
+      
+      destroy: function() {
+         this.worldBoundary.destroy();
+         this.gravity.destroy();
+         this.base();
+      },
+      
+      release: function() {
+         this.worldAABB = null;
+         this.gravity = null,
+         this.worldBoundary = null;
+         this.world = null;
+         this.base();
+      },
+      
+      update: function(renderContext, time) {
+         this.world.Step(1/Engine.getFPS(), 10);   
+      },
+      
+      /**
+       * Support method to get the ground body for the world.
+       * @return {b2Body} The world's ground body
+       * @private
+       */
+      getGroundBody: function() {
+         return this.world.GetGroundBody();
+      },
+      
+      /**
+       * Support method to add a body to the simulation.  The body must be one of the
+       * box2d-js body types.  This method is intended to be used by {@link SimulationObject}.
+       * 
+       * @param b2jsBody {b2Body} A box2d-js Body object
+       * @private
+       */
+      addBody: function(b2jsBody) {
+         return this.world.CreateBody(b2jsBody);
+      },
+      
+      /**
+       * Support method to add a joint to the simulation.  The joint must be one of the
+       * box2d-js joint types.  This method is intended to be used by {@link SimulationObject}.
+       * 
+       * @param b2jsJoint {b2Joint} A box2d-js Joint object
+       * @private
+       */
+      addJoint: function(b2jsJoint) {
+         return this.world.CreateJoint(b2jsJoint);
+      },
+      
+      /**
+       * Support method to remove a body from the simulation.  The body must be one of the
+       * box2d-js body types.  This method is intended to be used by {@link SimulationObject}.
+       * 
+       * @param b2jsBody {b2Body} A box2d-js Body object
+       * @private
+       */
+      removeBody: function(b2jsBody) {
+         this.world.DestroyBody(b2jsBody);
+      },
+      
+      /**
+       * Support method to remove a joint from the simulation.  The joint must be one of the
+       * box2d-js joint types.  This method is intended to be used by {@link SimulationObject}.
+       * 
+       * @param b2jsJoint {b2Joint} A box2d-js Joint object
+       * @private
+       */
+      removeJoint: function(b2jsJoint) {
+         this.world.DestroyJoint(b2jsJoint);
+      }     
+      
+   }, {
+      
+      getClassName: function() {
+         return "Simulation";
+      }
+      
+   });
 
-	return Simulation;
-	
+   return Simulation;
+   
 });
