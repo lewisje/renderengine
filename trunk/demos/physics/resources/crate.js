f64d6e2e5428a7af55957749e7e0dffbce599858
bfattori <bfattori@508b6656-1844-0410-a2e5-a95b844a0cf6>
2010-10-03 21:13:40 +0000

    * Added box bodies to physics demo

---
+++ trunk/demos/physics/resources/crate.js
@@ -0,0 +1,15 @@
+{
+   // Frame (f): left, top, frameWidth, frameHeight
+   // Animation (a): left, top, frameWidth, frameHeight, frameCount, speedMS, loop/toggle
+   "bitmapImage": "crate.png",
+   "bitmapWidth": 120,
+   "bitmapHeight": 60,
+   "sprites": {
+      "crate": {
+         "f" : [0, 0, 60, 60]
+      },
+      "over": {
+         "f" : [60, 0, 60, 60]
+      }
+   }
+}
