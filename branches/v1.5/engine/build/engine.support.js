edaca252b0296eb097d32fd3d12a76c178cd38eb
bfattori@gmail.com <bfattori@gmail.com@508b6656-1844-0410-a2e5-a95b844a0cf6>
2010-10-28 15:50:46 +0000

    * Added checks for Canvas support to sysInfo()

--- branches/v1.5/engine/build/engine.support.js
+++ branches/v1.5/engine/build/engine.support.js
@@ -413,6 +413,11 @@ var EngineSupport = Base.extend(/** @scope EngineSupport.prototype */{
       }
    },
 
+	/**
+	 * Determine the OS platform from the user agent string, if possible
+	 * @private
+    * @memberOf EngineSupport
+	 */
 	checkOS: function() {
 		// Scrape the userAgent to get the OS
 		var uA = navigator.userAgent.toLowerCase();
@@ -461,6 +466,18 @@ var EngineSupport = Base.extend(/** @scope EngineSupport.prototype */{
     *       <li>database - indexedDB storage is supported</li>
     *       </ul>
     *    </li>
+    *    <li>canvas:
+    *       <ul><li>emulated - Canvas support emulated by FlashCanvas</li>
+    *       <li>defined - Canvas is either native or emulated</li>
+    *       <li>text - Supports text</li>
+    *       <li>textMetrics - Supports text measurement</li>
+    *			<li>contexts:
+    *      		<ul><li>ctx2D - Supports 2D</li>
+    *				<li>ctxGL - Supports webGL</li>
+    *				</ul>
+    *			</li>
+    *       </ul>
+    *    </li>
     *    </ul>
     * </li>
     * </ul>
@@ -469,6 +486,48 @@ var EngineSupport = Base.extend(/** @scope EngineSupport.prototype */{
     */
    sysInfo: function() {
       if (!EngineSupport._sysInfo) {
+      	
+      	// Determine if the browser supports Canvas
+      	var canvasSupport = {
+      		emulated: false,
+      		defined: false,
+      		text: false,
+      		textMetrics: false,
+      		contexts: {
+      			ctx2D: false,
+      			ctxGL: false
+      		}
+      	};
+      	if (document.createElement) {
+      		// Standards browsers
+      		var canvas = document.createElement("canvas");
+      		if (typeof canvas != "undefined") {
+      			canvasSupport.defined = true;
+	      		var c2d = canvas.getContext("2d");
+	      		if (typeof c2d != "undefined") {
+	      			canvasSupport.contexts.ctx2D = true;
+	      			
+						// Does it support native text
+						canvasSupport.text = (typeof c2d.fillText == "function");
+						canvasSupport.textMetrics = (typeof c2d.measureText == "function");
+					}
+	      		
+	      		var webGL = canvas.getContext("glcanvas");
+	      		if (typeof webGL != "undefined") {
+	      			canvasSupport.contexts.ctxGL = true;
+	      		}
+	      	}
+      	}
+      	
+			if (typeof FlashCanvas != "undefined") {
+				// Emulation of canvas provided by FlashCanvas
+      		canvasSupport.emulated = true;
+      		canvasSupport.defined = true;
+      		canvasSupport.contexts.ctx2D = true;
+      		canvasSupport.text = true;
+      	}
+      
+      	// Build support object
          EngineSupport._sysInfo = {
             "browser" : $.browser.chrome ? "chrome" :
 				           ($.browser.android ? "android" :
@@ -497,9 +556,11 @@ var EngineSupport = Base.extend(/** @scope EngineSupport.prototype */{
                   "session" : (typeof sessionStorage !== "undefined"),
                   "database": (typeof indexedDB !== "undefined")
                } : null),
-               "geo": (typeof navigator.geolocation !== "undefined")
+               "geo": (typeof navigator.geolocation !== "undefined"),
+               "canvas" : canvasSupport
             }
          };
+         
          $(document).ready(function() {
             // When the document is ready, we'll go ahead and get the width and height added in
             EngineSupport._sysInfo = $.extend(EngineSupport._sysInfo, {
