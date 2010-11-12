11d93fdbdccfc97af4509b4dcd87ececdeb3497a
bfattori <bfattori@508b6656-1844-0410-a2e5-a95b844a0cf6>
2010-11-12 00:57:36 +0000

    * Merging with changes in v1.5.2

--- engine/build/engine.math2.js
+++ engine/build/engine.math2.js
@@ -58,7 +58,7 @@ var Math2 = Base.extend(/** @scope Math2.prototype */{
 	},
 	
 	/**
-	 * Get a random integer from the pseduo random number generator.
+	 * Returns a random integer between 0 and 4,294,967,296.
 	 * @return {Number} An integer between 0 and 2^32
 	 */
 	randomInt: function() {
@@ -67,12 +67,62 @@ var Math2 = Base.extend(/** @scope Math2.prototype */{
 	},
 	
 	/**
-	 * Get a random float between 0 (inclusive) and 1 (exclusive)
+	 * Returns a pseudo-random number between 0 (inclusive) and 1 (exclusive)
 	 * @return {Number} A number between 0 and 1
 	 */
 	random: function() {
 		// returns in range [0,1]
 		return this.randomInt() / (this.m - 1);
+	},
+	
+	/**
+	 * Return a random value within the <tt>low</tt> to <tt>height</tt> range,
+	 * optionally as an integer value only.
+	 *
+	 * @param low {Number} The low part of the range
+	 * @param high {Number} The high part of the range
+	 * @param [whole] {Boolean} Return whole values only
+	 * @return {Number}
+	 */
+	randomRange: function(low, high, whole) {
+		var v = low + (this.random() * high);
+		return (whole ? Math.floor(v) : v);
+	},
+	
+	/**
+	 * Parse a binary string into a number.
+	 * 
+	 * @param bin {String} Binary string to parse
+	 * @return {Number}
+	 */
+	parseBin: function(bin) {
+		if (!isNaN(bin)) {
+			return parseInt(bin, 2);
+		}
+	},
+	
+	/**
+	 * Converts a number to a hexidecimal string, prefixed by "0x".
+	 *
+	 * @param num {Number} The number to convert
+	 * @return {String}
+	 */
+	toHex: function(num) {
+		if (!isNaN(num)) {
+			return ("0x" + num.toString(16));
+		}
+	},
+	
+	/**
+	 * Converts a number to a binary string.
+	 *
+	 * @param num {Number} The number to convert
+	 * @return {String}
+	 */
+	toBinary: function(num) {
+		if (!isNaN(num)) {
+			return num.toString(2);
+		}
 	}
 });
 
